ac45367bcb476eb1fc86731be8aced18d317c622
sanela bektic <sanela.bektic@campus.lmu.de>
2024-04-02 15:23:43 +0200

    Initial commit

---
+++ src/main/java/com/example/springdemo/BeanCOnfigDemo.java
@@ -0,0 +1,14 @@
+package com.example.springdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanCOnfigDemo {
+
+ //   @Bean
+
+//   // public  TestCOntroller TestControler{
+   //     return new TestCOntroller();
+ //   }
+}
